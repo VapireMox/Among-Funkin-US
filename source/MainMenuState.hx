dccacb52f6bb08474d9819b8616e5d1f149d2103
GeXie19 <3449326235@qq.com>
2025-02-06 22:43:36 +0800

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -1,100 +1,91 @@
 package;
 
-import extra.SUSButton;
-import flixel.group.FlxGroup;
-import flixel.input.mouse.FlxMouseEventManager;
-import flixel.FlxG;
-import flixel.math.FlxPoint;
-import flixel.FlxSprite;
 import flixel.addons.display.FlxBackdrop;
-import openfl.utils.Assets;
-import openfl.utils.AssetType;
-
-import hscript.Interp;
-import hscript.Parser;
+import extra.SUSButton;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.group.FlxGroup;
+import flixel.group.FlxSpriteGroup;
 
 class MainMenuState extends MusicBeatState {
-  public static var psychEngineVersion:String = "0.6.3-special";
+  public static var psychEngineVersion:String = "0.6.3(AFUS)";
   
-  //星空
-  var starrySky:FlxBackdrop;
-  var panelBG:FlxSprite;
+  var bg:FlxSprite;
   var blank:FlxSprite;
   var charTopState:FlxSprite;
+  var starrySky:FlxSprite;
   var logo:FlxSprite;
 
-  var buttons:FlxTypedGroup<SUSButton>;
-  var asChars:Array<FlxSprite>;
-  
-  override function create() {
-    super.create();
+  var buttons:FlxTypedSpriteGroup<SUSButton>;
+  var susPeople:FlxGroup;
 
+  public override function create() {
     if(!FlxG.mouse.visible) FlxG.mouse.visible = true;
+    
+    super.create();
+    
+	  starrySky = new FlxBackdrop(Paths.image("mainmenu/StarrySky"));
+	  starrySky.setGraphicSize(FlxG.width, FlxG.height);
+	  add(starrySky);
+	
+	  susPeople = new FlxGroup();
+	  createSUSPeople();
+	  add(susPeople);
 
-    starrySky = new FlxBackdrop(Paths.image("mainmenu/StarrySky"));
-    starrySky.setGraphicSize(FlxG.width, FlxG.height);
-    starrySky.updateHitbox();
-    starrySky.scrollFactor.set();
-    add(starrySky);
-
-    panelBG = new FlxSprite().loadGraphic(Paths.image("mainmenu/panelBG"));
-    panelBG.setGraphicSize(FlxG.width, FlxG.height);
-    panelBG.updateHitbox();
-    panelBG.scrollFactor.set();
-    add(panelBG);
-
-    blank = new FlxSprite(-65, 75).loadGraphic(Paths.image("mainmenu/blank"));
-    blank.scrollFactor.set();
-    blank.scale.set(0.34, 0.34);
-    blank.updateHitbox();
-    add(blank);
-
-    buttons = new FlxTypedGroup<SUSButton>();
-    createButtons();
-    add(buttons);
-
-    charTopState = new FlxSprite(-4, -51).loadGraphic(Paths.image("mainmenu/CharTopState"));
-    charTopState.scrollFactor.set();
-    charTopState.scale.set(0.262, 0.2);
-    charTopState.updateHitbox();
-    add(charTopState);
-
-    logo = new FlxSprite(0, -40).loadGraphic(Paths.image("mainmenu/MainLOGO"));
-    logo.scrollFactor.set();
-    logo.scale.set(0.16, 0.16);
-    logo.updateHitbox();
-    logo.x = FlxG.width - logo.width;
-    add(logo);
+	  bg = new FlxSprite().loadGraphic(Paths.image("mainmenu/panelBG"));
+	  bg.setGraphicSize(FlxG.width, FlxG.height);
+	  bg.updateHitbox();
+	  add(bg);
+	
+	  blank = new FlxSprite(-65, 81).loadGraphic(Paths.image("mainmenu/blank"));
+	  blank.scale.set(0.333, 0.333);
+	  blank.updateHitbox();
+	  add(blank);
+	
+	  charTopState = new FlxSprite(-8, -50).loadGraphic(Paths.image("mainmenu/CharTopState"));
+	  charTopState.scale.set(0.264, 0.2);
+	  charTopState.updateHitbox();
+	  add(charTopState);
+	
+	  logo = new FlxSprite(FlxG.width, -25).loadGraphic(Paths.image("mainmenu/MainLOGO"));
+	  logo.scale.set(0.15, 0.15);
+	  logo.updateHitbox();
+	  logo.x -= logo.width;
+	  add(logo);
+	
+	  buttons = new FlxTypedSpriteGroup<SUSButton>(blank.x + 92.5, blank.y + 85);
+	  createButtons();
+	  add(buttons);
   }
 
-  override function update(elapsed:Float) {
-    super.update(elapsed);
+  public override function update(elapsed:Float) {
+	  starrySky.x += elapsed * 2;
   }
 
-  private function createButtons() {
-    var buttonNames:Array<String> = ["SM", "FP", "SP", "PE", "CM", "OS", "CS", "LE"];
-    var jiange:Float = -7.5;
+  function createButtons() {
+	  var buttonNames:Array<String> = ["SM", "FP", "SP", "PE", "CM", "OS", "CS", "LE"];
+	
+	  for(num=>buttonName in buttonNames) {
+		  var button = new SUSButton(0, 0, Paths.image('mainmenu/${buttonName}Button'));
+		  button.scale.set(0.333, 0.333);
+		  button.updateHitbox();
+		  if(num != buttonNames.indexOf("LE"))
+			  button.y = num * (num > 0 ? buttons.members[buttons.length - 1].height : 0);
+		
+		  if(num < buttonNames.indexOf("PE") && num > 0) button.y -= (num % buttonNames.indexOf("PE")) * 10;
+		  else if(num >= buttonNames.indexOf("PE") && num < buttonNames.indexOf("LE")) {
+			  button.y -= 6.5;
+			  if(num > buttonNames.indexOf("PE")) button.y -= 20 * (num % (buttonNames.indexOf("LE") - buttonNames.indexOf("PE")) + 1);
+		  }else if(num != 0) {
+			  button.y = buttonNames.indexOf("CS") * button.height - 67;
+		  }
+		
+		  if(num > buttonNames.indexOf("OS") && num <= buttonNames.indexOf("LE")) {
+			  button.x += 78 * (num == buttonNames.indexOf("CS") ? -1 : 1);
+		  }
+		  buttons.add(button);
 
-    for(num=>buttonName in buttonNames) {
-      var susButton:SUSButton = new SUSButton(35, 162, Paths.image('mainmenu/${buttonName}Button'));
-      susButton.scale.set(0.33, 0.33);
-      susButton.updateHitbox();
-
-      var pointer = FlxPoint.get((switch(buttonName) {
-        case "CS": -75;
-        case "LE": 75;
-        default: 0;
-      }), (switch(buttonName) {
-        case "LE": 63.5 * (num - 1) + 50;
-        case "PE" | "CM" | "OS" | "CS": 63.5 * num + 65;
-        default: 63.5 * num;
-      }));
-      susButton.x += pointer.x;
-      susButton.y += pointer.y;
-
-      buttons.add(susButton);
-
-      susButton.clickCallback = () -> {
+      button.clickCallback = () -> {
         switch(buttonName) {
           case "SM":
             MusicBeatState.switchState(new StoryMenuState());
@@ -109,7 +100,55 @@ class MainMenuState extends MusicBeatState {
           default: 
             lime.app.Application.current.window.alert("Feature Not Free", "error");
         }
-      };
-    }
+      }; 
+	  }
+  }
+
+  function createSUSPeople() {
+	  var susMans:Array<String> = ["blue-half", "diamondPurple", "orange", "purple", "sadRed", "wazeGrey"];
+	
+	  var list = FlxG.random.int(1, 2);
+	  if(list > 0)
+		  for(i in 0...list) {
+			  var dataS:Int = FlxG.random.int(0, susMans.length - 1)
+			  var susMan:String = susMans[dataS];
+			  var sb = new FlxSprite().loadGraphic(Paths.image('mainmenu/characters/${susMan}'));
+			  sb.scale.set(0.2, 0.2);
+			  sb.updateHitbox();
+			
+			  var dataX = FlxG.random.int(0, 1);
+			  var dataAngle:Int = FlxG.random.int(-1, 1);
+			  sb.x = dataX * FlxG.width;
+			  sb.y = FlxG.random.float(140, 580);
+			  sb.angle = FlxG.random.int(0, 360);
+			  susPeople.add(sb);
+			
+			  var angleSpeed:Int = FlxG.random.int(0, 180);
+			
+			  FlxTween.tween(sb, {
+				  x: (1 - dataX) * FlxG.width,
+				  y: FlxG.random.float(140, 580)
+			  }, FlxG.random.float(8, 15), {
+				  onComplete: function(tween:FlxTween) {
+					  susPeople.remove(sb, true);
+					  createSUSPeople();
+				  },
+				  onUpdate: function(tween:FlxTween) {
+					  switch(susMan) {
+						  case "sadRed":
+							  //sb.origin.set(FlxG.random.float(-2.5, 2.5), FlxG.random.float(-2.5, 2.5));
+							  //sb.updateHitbox();
+						  default:
+							  sb.angle += dataAngle * (FlxG.elapsed * angleSpeed);
+					  }
+				  }
+			  });
+			
+			  if(susPeople.length > 4) {
+				  for(i in 0...susPeople.length - 4) {
+					  susPeople.remove(susPeople.members[susPeople.members.length - 1], true);
+				  }
+			  }
+		  }
   }
 }
