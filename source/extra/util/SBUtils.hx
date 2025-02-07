aba46ca9951171b602a8ad481bb180fc0480f155
GeXie19 <3449326235@qq.com>
2025-02-07 12:46:22 +0800

    Create SBUtils.hx

---
+++ source/extra/util/SBUtils.hx
@@ -0,0 +1,9 @@
+package extra.util;
+
+import flixel.FlxBasic;
+
+class SBUtils extends FlxBasic {
+  public static function inputReflect(str:String, wait:Float = 1, func():Void->Void) {
+    
+  }
+}
