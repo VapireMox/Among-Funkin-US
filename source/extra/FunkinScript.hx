c45672ab5c836d7de4fa55eb24dbfb4d9d0761f7
GeXie19 <3449326235@qq.com>
2025-02-13 08:50:58 +0800

    Create FunkinScript.hx

---
+++ source/extra/FunkinScript.hx
@@ -0,0 +1,41 @@
+package extra;
+
+import haxe.io.Path;
+import haxe.ds.StringMap;
+
+class FunkinScript {
+  public static function getProcessedFields(parent:FunkinScript):Map<String, Dynamic> {
+    var sb:StringMap = new StringMap();
+    sb.set("psychEngineVersion", MainMenuState.psychEngineVersion);
+
+    return sb;
+  }
+  
+  public var scriptName:String;
+  public var sctiptExtension:String;
+  public var path:String;
+  public var directory:String;
+  
+  public function new(path:String) {
+    this.path = path;
+    scriptName = Path.withoutDirectory(path);
+    scriptExtension = Path.extension(path);
+    directory = Path.directory(path);
+  }
+
+  public function set(name:String):Void {
+  }
+
+  public function get(name:String, val:Dynamic):Dynamic {
+    return null;
+  }
+
+  public function call(name:String, ?args:Dynamic):Dynamic {
+    return null;
+  }
+
+  @:noCompletion
+  function toString():String {
+    return '{Path: $path}';
+  }
+}
